0ccba1ad5eb3b311e70cc326c0c9032f589bf10e
Rossy Lopez <rossy181071@hotmail.com>
2024-05-12 23:20:18 -0600

    Initial commit

--- src/my-element.js
+++ src/my-element.js
@@ -1,6 +1,4 @@
 import { LitElement, css, html } from 'lit'
-import litLogo from './assets/lit.svg'
-import viteLogo from '/vite.svg'
 
 /**
  * An example element.
@@ -12,120 +10,197 @@ export class MyElement extends LitElement {
   static get properties() {
     return {
       /**
-       * Copy for the read the docs hint.
+       * Este es el titulo de mi pagina
        */
-      docsHint: { type: String },
-
+      title: { type: String },
       /**
-       * The number of times the button has been clicked.
+       * Este es mi parrafo 1
        */
-      count: { type: Number },
+      paragraph1: { type: String },
+      /**
+       * Esta es la ur de la imagen 1
+       */
+      Image1Url: { type: String },
+      /**
+       * Este es el elemento 1 de la lista 1
+       */
+      list1Element1: { this: String },
+      /**
+       * Este es el elemento 2 de la lista 1
+       */
+      list1Element2: { this: String },
+      /**
+       * Este es el elemento 3 de la lista 1
+       */
+      list1Element3: { this: String },
+      /**
+       * Este es el elemento 4 de la lista 1
+       */
+      list1Element4: { this: String },
+      /**
+       * Este es el elemento  de la lista 1
+       */
+      list1Element5: { this: String },
+      /**
+       * Esta es la url de la imagen 2 
+       */
+      image2Url: { type: String },
+      
     }
   }
 
   constructor() {
     super()
-    this.docsHint = 'Click on the Vite and Lit logos to learn more'
-    this.count = 0
+   
+    this.title = 'BIENVENIDOS AGENCIA DE VIAJES VLADYL'
+    this.paragraph1 = 'VIAJES A MEXICO Y EL MUNDO'
+    this.image1Url = 'https://s2.abcstatics.com/abc/www/multimedia/viajar/2023/10/18/suite-RdgxRJigYrVhHngiaOPamFM-1200x840@abc.jpg'
+    this.paragraph2 = 'OFRECEMOS DIFERENTES PAQUETES '
+    this.paragraph3 = 'PAQUETE 1'
+    this.image2Url = 'https://i.ytimg.com/vi/NDNPo5xcbj8/maxresdefault.jpg'
+    this.list1Element1 = 'Hotel'
+    this.list1Element2 = 'Comida'
+    this.list1Element3 = 'Transporte'
+    this.list1Element4 = 'Bebidas'
+    this.list1Element5 = 'Diversion ni;os y adultos'
+    this.paragraph4 = 'PAQUETE 2'
+    this.image3Url = 'https://i.pinimg.com/originals/fd/dd/59/fddd59b6821afefff878de01615d48a8.png'
+    this.list2Element1 = 'Hotel'
+    this.list2Element2 = 'Comida y Bebidas Nacionales'
+    this.list2Element3 = 'Transporte'
+    this.list2Element4 = 'Noche de baile'
+    this.list2Element5 = 'Guarderia'
+    this.paragraph5 = 'PAQUETE 3'
+    this.image4Url = 'https://www.shutterstock.com/image-photo/kids-jump-into-sea-yacht-260nw-1943031181.jpg'
+    this.list3Element1 = 'Hotel'
+    this.list3Element2 = 'Comida y Bebidas Nacionales'
+    this.list3Element3 = 'Transporte'
+    this.list3Element4 = 'Viaje en Yate'
+    this.list3Element5 = 'Guarderia'
+    this.paragraph6 = 'PAQUETE 4'
+    this.image5Url = 'https://blog.garrafon.com.mx/wp-content/uploads/2023/01/tirolesa-en-isla-mujeres.jpg'
+    this.list4Element1 = 'Hotel'
+    this.list4Element2 = 'Comida y Bebidas Nacionales'
+    this.list4Element3 = 'Transporte'
+    this.list4Element4 = 'Tirolesa'
+    this.list4Element5 = 'Guarderia'
+
   }
 
   render() {
     return html`
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src=${viteLogo} class="logo" alt="Vite logo" />
-        </a>
-        <a href="https://lit.dev" target="_blank">
-          <img src=${litLogo} class="logo lit" alt="Lit logo" />
-        </a>
-      </div>
-      <slot></slot>
-      <div class="card">
-        <button @click=${this._onClick} part="button">
-          count is ${this.count}
-        </button>
-      </div>
-      <p class="read-the-docs">${this.docsHint}</p>
-    `
-  }
+    <div id="Agencia de Viajes Vladyl">
+    <h1>${this.title}</h1>
+    <p>${this.paragraph1}</p>
+    <img src="${this.image1Ur}">
+    <p>${this.paragraph2}</p>
+    <img src="${this.image2Url}">
+    <p>${this.paragraph3}</p>
+    <ul>
+      <li><i>${this.list1Element1}</i></li>
+      <li><i>${this.list1Element2}</i></li>
+      <li><i>${this.list1Element3}</i></li>
+      <li><i>${this.list1Element4}</i></li>
+      <li><i>${this.list1Element5}</i></li>
+    </ul>
+    <p>${this.paragraph4}</p>
+    <img src="${this.image3Url}">
+    <li><i>${this.list2Element1}</i></li>
+    <li><i>${this.list2Element2}</i></li>
+    <li><i>${this.list2Element3}</i></li>
+    <li><i>${this.list2Element4}</i></li>
+    <li><i>${this.list2Element5}</i></li>
+    <p>${this.paragraph5}</p>
+    <img src="${this.image4Url}">
+    <li><i>${this.list3Element1}</i></li>
+    <li><i>${this.list3Element2}</i></li>
+    <li><i>${this.list3Element3}</i></li>
+    <li><i>${this.list3Element4}</i></li>
+    <li><i>${this.list3Element5}</i></li>
+    <p>${this.paragraph6}</p>
+    <img src="${this.image5Url}">
 
-  _onClick() {
-    this.count++
-  }
+
+    <li><i>${this.list4Element1}</i></li>
+    <li><i>${this.list4Element2}</i></li>
+    <li><i>${this.list4Element3}</i></li>
+    <li><i>${this.list4Element4}</i></li>
+    <li><i>${this.list4Element5}</i></li>
+
+
+  </div> `
+  
+}
+
 
   static get styles() {
     return css`
       :host {
-        max-width: 1280px;
+        max-width: 1100px;
         margin: 0 auto;
-        padding: 2rem;
-        text-align: center;
+        padding: rem;
+        
+      }
+      body{
+        background-color: plum;
+      }
+      #parrafo {
+        background-color: chartreuse;
+      }
+      #parrafo {
+        font-family: Georgia
+      }
+      #parrafo { 
+        color: rgb(165, 42, 144);
+      }
+      #parrafo {
+        font-size:8vh;
+      }  
+      
+      #viajes {
+        background-color: rgb(187, 109, 109);
+        font-family: Times;
+        color: blue;
+      }
+      
+      div{
+    padding: 7vw;
+    }
+    #viajes a mexico y el mundo {
+      background-color: cadetblue;
+    }
+      img {
+    width:25vw;
+      }
+      p {
+    font-size:3vw;
       }
 
-      .logo {
-        height: 6em;
-        padding: 1.5em;
-        will-change: filter;
-        transition: filter 300ms;
-      }
-      .logo:hover {
-        filter: drop-shadow(0 0 2em #646cffaa);
-      }
-      .logo.lit:hover {
-        filter: drop-shadow(0 0 2em #325cffaa);
-      }
-
-      .card {
-        padding: 2em;
-      }
-
-      .read-the-docs {
-        color: #888;
-      }
-
-      a {
-        font-weight: 500;
-        color: #646cff;
-        text-decoration: inherit;
-      }
-      a:hover {
-        color: #535bf2;
-      }
-
-      ::slotted(h1) {
-        font-size: 3.2em;
-        line-height: 1.1;
-      }
-
-      button {
-        border-radius: 8px;
-        border: 1px solid transparent;
-        padding: 0.6em 1.2em;
-        font-size: 1em;
-        font-weight: 500;
-        font-family: inherit;
-        background-color: #1a1a1a;
-        cursor: pointer;
-        transition: border-color 0.25s;
-      }
-      button:hover {
-        border-color: #646cff;
-      }
-      button:focus,
-      button:focus-visible {
-        outline: 4px auto -webkit-focus-ring-color;
-      }
-
-      @media (prefers-color-scheme: light) {
-        a:hover {
-          color: #747bff;
-        }
-        button {
-          background-color: #f9f9f9;
-        }
-      }
+    #titulo-1 {
+      color: coral;
+    }
+    #titulo-2 {
+      color: aqua;
+    }
+    #titulo-3 {
+      color: chartreuse;
+    }
+    #titulo-4 {
+      color: brown;
+    }
+    #titulo-5 {
+      color: darkgreen;
+    }
+    #titulo-6 {
+      color: darkorchid;
+    }
+    li{
+    font-size: 7vw;
+    }
     `
   }
 }
 
+
 window.customElements.define('my-element', MyElement)
+
